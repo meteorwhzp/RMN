0b1a23fda5bd12467b3ac1cebaa1a190670cf06b
meteorwhzp <meteorwhzp@github.com>
2017-10-27 21:27:59 +0800

    init

---
+++ src/client/goap_client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/dustin/go-coap"
+	//"time"
+	"strconv"
+	"time"
+	"fmt"
+)
+
+func main() {
+
+	req := coap.Message{
+		Type:      coap.Confirmable,
+		Code:      coap.GET,
+		MessageID: 12345,
+		Payload:   []byte("hello, world!"),
+	}
+
+	path := "/some/path"
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+	num, _ := strconv.Atoi(os.Args[2])
+	req.SetOption(coap.ETag, "weetag")
+	req.SetOption(coap.MaxAge, 3)
+	req.SetPathString(path)
+
+	c, err := coap.Dial("udp", "localhost:5684")
+	if err != nil {
+		log.Fatalf("Error dialing: %v", err)
+	}
+
+	startTime := time.Now()
+	for i := 0; i < num; i++ {
+		rv, err := c.Send(req)
+		if err != nil {
+			log.Printf("%s", err)
+		}
+		if rv != nil {
+			log.Printf("%s", rv.Payload)
+		}
+	}
+	endTimeCoAP := time.Now()
+	fmt.Println("coap cost: ", endTimeCoAP.Sub(startTime).Nanoseconds()/int64(time.Millisecond), "ms")
+
+}
---
+++ src/server/coap_server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/dustin/go-coap"
+	"time"
+	"fmt"
+)
+
+var startTime int64
+
+func handleA(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
+	timeNow := time.Now().UnixNano() / 1000000
+	spendTime := timeNow - startTime
+	fmt.Println("speed time :", spendTime)
+	//log.Printf("Got message in handleA: path=%q: %#v from %v", m.Path(), m, a)
+	if m.IsConfirmable() {
+		res := &coap.Message{
+			Type:      coap.Acknowledgement,
+			Code:      coap.Content,
+			MessageID: m.MessageID,
+			Token:     m.Token,
+			Payload:   []byte("hello world"),
+		}
+		res.SetOption(coap.ContentFormat, coap.TextPlain)
+
+		//log.Printf("Transmitting from A %#v", res)
+		return res
+	}
+	return nil
+}
+
+func handleB(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message {
+	timeNow := time.Now().UnixNano() / 1000000
+	spendTime := timeNow - startTime
+	fmt.Println("speed time :", spendTime)
+	log.Printf("Got message in handleB: path=%q: %#v from %v", m.Path(), m, a)
+	if m.IsConfirmable() {
+		res := &coap.Message{
+			Type:      coap.Acknowledgement,
+			Code:      coap.Content,
+			MessageID: m.MessageID,
+			Token:     m.Token,
+			Payload:   []byte("good bye!"),
+		}
+		res.SetOption(coap.ContentFormat, coap.TextPlain)
+
+
+		//log.Printf("Transmitting from B %#v", res)
+		return res
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("start server")
+	startTime = time.Now().UnixNano() / 1000000
+	mux := coap.NewServeMux()
+	mux.Handle("/a", coap.FuncHandler(handleA))
+	mux.Handle("/b", coap.FuncHandler(handleB))
+
+	log.Fatal(coap.ListenAndServe("udp", ":5684", mux))
+}
